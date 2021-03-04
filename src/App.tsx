eed123bfe1eb03d4514c0fc4378e9d490d9b89f4
moyongkexing <moyongkexing@gmail.com>
2021-03-04 10:07:35 +0900

    Feedコンポーネントにレスポンシブを追加

--- src/App.tsx
+++ src/App.tsx
@@ -30,7 +30,7 @@ const App: React.FC = () => {
   return (
     <>
       {user.uid ? (
-        <div className="flex justify-center h-screen bg-appBg px-64">
+        <div className="flex justify-center h-screen bg-appBg 2xl:px-64 px-32">
           <Feed />
         </div>
       ) : (
